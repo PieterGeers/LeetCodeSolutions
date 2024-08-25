41bbace2ed6ed362ea6a73deba1f71fe242f0678
Pieter <p.geers3@gmail.com>
2024-08-25 19:12:12 +0200

    Commit solution for problem 2

---
+++ Solutions/1-99/02_AddTwoNumbers.cs
@@ -0,0 +1,66 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace Solutions._1_99
+{
+    internal class _02_AddTwoNumbers
+    {
+        public _02_AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = AddTwoNumbers(l1, l2);
+
+            Console.WriteLine(result.ToString());
+        }
+
+        private ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode dummyHead = new ListNode(0);
+            ListNode tail = dummyHead;
+            int carry = 0;
+
+            while (l1 != null || l2 != null || carry != 0)
+            {
+                int n1 = l1?.val ?? 0;
+                int n2 = l2?.val ?? 0;
+
+                int sum = (n1 + n2 + carry);
+                carry = sum / 10;
+
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+
+            return dummyHead.next;
+        }
+    }
+
+    internal sealed class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        public override string ToString()
+        {
+            ListNode node = this;
+
+            string result = string.Empty;
+
+            while (node?.val != null)
+            {
+                result = string.IsNullOrEmpty(result) ? $"{node.val}" : $"{result}, {node.val}";
+                node = node.next;
+            }
+
+            return result;
+        }
+    }
+}
--- Solutions/Program.cs
+++ Solutions/Program.cs
@@ -1,3 +1,5 @@
 ﻿using Solutions._1_99;
 
-new _1_TwoSum([2, 7, 11, 15], 9);
+var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+new _02_AddTwoNumbers(l1, l2);
